93fc1956ac5e747b348d6cd61b51612f50230a7d
Mitchell Wolfe <mitchell.wolfe@colorado.edu>
2014-06-30 17:07:37 -0600

    Scroll to initial implementation

--- javascript/main.js
+++ javascript/main.js
@@ -8,15 +8,18 @@ $(window).scroll(function() {
 
 function barSquish(scroll) {
 	var current_Scroll = scroll;
-	if(current_Scroll < 61) {
-		var bar_height = $(window).scroll(function() {
-			var current_height = $("#top_bar").css("height");
-			return current_height;
-		});
-
-		$("#top_bar").css("height", 160 - current_Scroll);
-		$("#name").css("top", 15 - (current_Scroll/2));
-		$("#nav-container").css(20 - (current_Scroll/2));
+	if(current_Scroll < 101) { 
+		$("#top_bar").css("height", 200 - current_Scroll);
+		$("#name").css("top", 30 - (current_Scroll/2));
+		$("#nav-container").css("top", 70 - (current_Scroll/2));
 	}
 }
 
+/*----------------------------------------------*/
+/* Scroll to */
+/*----------------------------------------------*/
+function scrollToElement(element_name) {
+	$("#element_name").on('click', animate({
+		scrollTop: $("#element_name").offset().top
+	}, 1000));
+}
