af9fcaa3a8b86a1ee6f6caddbc50f5848f6f7e07
Mitchell Wolfe <mitchell.wolfe@colorado.edu>
2014-07-01 13:25:40 -0600

    Javascript scrolling, CSS area changes

--- javascript/main.js
+++ javascript/main.js
@@ -8,18 +8,9 @@ $(window).scroll(function() {
 
 function barSquish(scroll) {
 	var current_Scroll = scroll;
-	if(current_Scroll < 101) { 
+	if(current_Scroll < 121) { 
 		$("#top_bar").css("height", 200 - current_Scroll);
 		$("#name").css("top", 30 - (current_Scroll/2));
 		$("#nav-container").css("top", 70 - (current_Scroll/2));
 	}
-}
-
-/*----------------------------------------------*/
-/* Scroll to */
-/*----------------------------------------------*/
-function scrollToElement(element_name) {
-	$("#element_name").on('click', animate({
-		scrollTop: $("#element_name").offset().top
-	}, 1000));
 }
