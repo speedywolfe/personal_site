c432c809e4054df48b9d64701ca337cb503d0141
Mitchell Wolfe <mitchell.wolfe@colorado.edu>
2014-07-02 09:35:48 -0600

    Initial Personal Sliding Algorithm

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,7 @@
 var window_width = window.innerWidth;
-var accordion_width = (window_width - 50);
+var accordion_width = (window_width - ((1*window_width)/10));
+var currert_panel_work = 1; // current panel of work slider
+var currert_panel_fun = 1;  // current panel of play slider
 
 /*----------------------------------------------*/
 /* Top Bar Squishing and growing based on Scroll*/
@@ -22,28 +24,10 @@ function barSquish(scroll) {
 /* Sliding Content Panels*/
 /*----------------------------------------------*/
 $(window).ready(function() {
-	$('#content1').liteAccordion({
-		containerWidth: accordion_width,
-		containerHeight: 700,
-		headerWidth: 60,
-		theme: 'dark'
-	});
-});
-
-$(window).ready(function() {
-	$('#content2').liteAccordion({
-		containerWidth: accordion_width,
-		containerHeight: 700,
-		headerWidth: 60,
-		theme: 'dark'
-	});
-})
-
-$(window).ready(function() {
 	$('#content3').liteAccordion({
 		containerWidth: accordion_width,
-		containerHeight: 700,
-		headerWidth: 60,
+		containerHeight: 680,
+		headerWidth: 50,
 		theme: 'dark'
 	});
 })
@@ -51,8 +35,36 @@ $(window).ready(function() {
 $(window).ready(function() {
 	$('#content4').liteAccordion({
 		containerWidth: accordion_width,
-		containerHeight: 700,
+		containerHeight: 680,
 		headerWidth: 60,
 		theme: 'dark'
 	});
 })
+
+/*----------------------------------------------*/
+/* Own Sliding Panels */
+/*----------------------------------------------*/
+function horizontal_slide(panel_number, which_panel) {
+	// offset will be one window width for each panel
+	var slide_offset = panel_number * window_width;
+
+	// decide on work or play panel
+	if(which_panel == 'work') {
+		current_panel_number = currert_panel_work;
+	}
+	if(which_panel == 'play') {
+		current_panel_number = currert_panel_fun;
+	}
+
+	// slide panel, direction depends on panel number
+	if(slide_offset > current_panel_number) {
+		$('work_content').animate({
+			'left' : slide_offset;
+		});
+	}
+	else {
+		$(this).animate({
+			'right' : slide_offset;
+		});
+	}
+}
