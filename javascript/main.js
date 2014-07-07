57c0d1a22136d6d4e8fa6a24f122a3b8e34f6664
Mitchell Wolfe <mitchell.wolfe@colorado.edu>
2014-07-07 17:03:45 -0600

    CSS refactor + add consistency

--- javascript/main.js
+++ javascript/main.js
@@ -9,7 +9,7 @@ function barSquish(scroll) {
 	if(current_Scroll < 121) { 
 		$("#top_bar").css("height", 200 - current_Scroll);
 		$("#name").css("top", 30 - (current_Scroll/2));
-		$("#nav-container").css("top", 70 - (current_Scroll/2));
+		$("#nav_container").css("top", 70 - (current_Scroll/2));
 	}
 }
 $(window).scroll(function() {
