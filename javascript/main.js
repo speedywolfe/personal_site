e52794e91e385951637e23dc087deaf915fa8ca7
Mitchell Wolfe <mitchell.wolfe@colorado.edu>
2014-07-07 10:48:10 -0600

    Jade re-factoring to separte files

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,9 @@
-var window_width = window.innerWidth;
-var accordion_width = (window_width - ((window_width)/10));
 var current_panel_work = 1; // current panel of work slider
 var current_panel_fun = 1;  // current panel of play slider
 
 /*----------------------------------------------*/
 /* Top Bar Squishing and growing based on Scroll*/
 /*----------------------------------------------*/
-$(window).scroll(function() {
-	var scroll_value = $(document).scrollTop().valueOf();
-	barSquish(scroll_value); 
-})
-
 function barSquish(scroll) {
 	var current_Scroll = scroll;
 	if(current_Scroll < 121) { 
@@ -19,22 +12,35 @@ function barSquish(scroll) {
 		$("#nav-container").css("top", 70 - (current_Scroll/2));
 	}
 }
+$(window).scroll(function() {
+	var scroll_value = $(document).scrollTop().valueOf();
+	barSquish(scroll_value); 
+});
 
 /*----------------------------------------------*/
 /* Panel Switching through transparency */
 /*----------------------------------------------*/
+function transparency_change(direction, panel_section, panel_number) {
+	if(direction === 'opaque') {
+		$('#' + panel_section + panel_number).animate({opacity: 1}, 1000);
+	}
+	else {
+		$('#' + panel_section + panel_number).animate({opacity: 0}, 1000);
+	}
+}
+
 function switch_panel(section, panel_number) {
 	// decide on work or play panel
 	var current_panel_number;
-	if(section == 'work_panel') {
+	if(section === 'work_panel') {
 		current_panel_number = current_panel_work;
 	}
-	if(section == 'fun_panel') {
+	if(section === 'fun_panel') {
 		current_panel_number = current_panel_fun;
 	}
 
 	// check if current displayed panel is same as selected
-	if(panel_number == current_panel_number) {/* Dont do anything */}
+	if(panel_number === current_panel_number) {/* Dont do anything */}
 
 	// actually switch panels if you click one that isn't currently displayed
 	else {
@@ -46,20 +52,11 @@ function switch_panel(section, panel_number) {
 		
 
 		// Have to check again which section we are in to update panel number
-		if(section == 'work_panel') {
+		if(section === 'work_panel') {
 			current_panel_work = panel_number;
 		}
-		if(section == 'fun_panel') {
+		if(section === 'fun_panel') {
 			current_panel_fun = panel_number;
 		}
-	}
-}
-
-function transparency_change(direction, panel_section, panel_number) {
-	if(direction == 'opaque') {
-		$('#' + panel_section + panel_number).animate({opacity: 1}, 1000);
-	}
-	else {
-		$('#' + panel_section + panel_number).animate({opacity: 0}, 1000);
 	}
 }
